34835dc6f93e18d17a22da6f6c37f3605291f2e9
Louie Heaton <louie@soboldltd.com>
2023-06-01 10:03:27 +0100

    build: structure adjustment

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>byDigital Best Practices</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/by-Digital/best-practices',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/by-Digital',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'byDigital Open Coding Standards',
   },
 }
 
